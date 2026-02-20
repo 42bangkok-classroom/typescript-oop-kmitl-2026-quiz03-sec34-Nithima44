580e4608b7f22d8dc0b09f15d61cb39701aa3288
Nithima44 <nitima7859@gmail.com>
2026-02-20 18:08:12 +0700

    car.ts

--- src/practicals/p03/character.ts
+++ src/practicals/p03/character.ts
@@ -1,1 +1,25 @@
-export class Character {}
+export class Character {
+    name?: string;
+    health?: number=100;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    getName(){
+        return this.name;
+    }
+    getHealth(){
+        return this.health;
+    }
+    receiveDamage(damage: number){
+        if(this.health){
+            this.health -= damage;
+        }
+    }
+}
+const character = new Character ("NPC");
+
+console.log(character.getName());
+console.log(character.getHealth());
+console.log(character.receiveDamage(10));
--- src/practicals/p03/warrior.ts
+++ src/practicals/p03/warrior.ts
@@ -2,4 +2,15 @@ import { Character } from "./character";
 
 export class Warrior extends Character {
   // TODO: implement class properties, constructor with super(...), and methods
+  weapon?: string;
+
+  constructor(name?: string, weapon?: string, health?: number) {
+    super();
+    this.name = name;
+    this.weapon = weapon;
+    this.health = health;
+  }
+  getWeapon() {
+    return this.weapon;
+  }
 }
