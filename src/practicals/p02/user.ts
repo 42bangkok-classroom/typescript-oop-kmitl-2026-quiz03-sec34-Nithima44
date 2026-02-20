a81400839a7d055496ec896b9d5a368e768a54ca
Nithima44 <nitima7859@gmail.com>
2026-02-20 17:41:54 +0700

    user.ts

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,22 @@
-export class User {}
+export class User {
+    static BIRTH_YEAR = 2000;
+    firstname?: string;
+    lastname?: string
+    private age?: number;
+
+    setFirstname(firstname: string) {
+        this.firstname = firstname;
+    }
+    setLastname(Lastname: string) {
+        this.lastname = Lastname;
+    }
+    setAge(age: number) {
+        this.age = age;
+    }
+    getAge(){
+        return this.age;
+    }
+    getFullName(){
+        return `${this.firstname} ${this.lastname}`;
+    }
+}
